539c7231af86c9ce4a3a624e34c33962f56c87b5
rajaaaaaaax <premwairagade001@gmail.com>
2024-10-22 10:18:43 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,205 +1,80 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import AnimeCarousel from "@/components/shared/anime-carousel";
-import { useGetAllAnime } from "@/lib/query-api";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { TopSearchItems } from "@/constants";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
-import ReactSimplyCarousel from "react-simply-carousel";
-import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-import { buttonVariants } from "@/components/ui/button";
-import AiringAnime from "@/components/airing-anime";
-import { MdArrowForwardIos } from "react-icons/md";
-import HomeLoading from "@/components/loaders/home-loading";
-import ScheduleAnime from "@/components/shared/schedule-anime";
-import RecentlyWatched from "@/components/shared/recently-watched";
-import LatestAnimes from "@/components/shared/latest-animes";
+import { SearchIcon } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 
-const HomePage = () => {
-  const { data, isLoading, isError, fetchStatus } = useGetAllAnime();
-
-  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-  const [id, setId] = useState<string[]>([])
-
-  useEffect(() => {
-    if(data?.latestEpisodeAnimes) {
-      const ids = data.latestEpisodeAnimes.map(anime => anime.id)
-      setId(ids)
-    }
-  }, [data?.latestEpisodeAnimes])
-
-  if (isLoading && !data) return <HomeLoading />;
-
-  if(isError || data?.status) {
-    return (
-      <div className="h-[calc(100lvh-140px)] w-full flex items-center justify-center">
-        <div className="">
-          <img src="/assets/empty.gif" alt="404 not found" className="h-56 w-72 object-contain" />
-          <h1 className="text-center text-2xl">{data?.status} {data?.message}</h1>
-          <p className="text-center">
-            Checkout this{" "}
-            <a className="text-primary underline" href="https://anifire-beta.vercel.app/">AniFire Beta</a>
-          </p>
-        </div>
-      </div>
-    )
-  };
+const Home = () => {
+  const [query, setQuery] = useState<string>("");
 
   return (
-    <div className="max-w-screen-2xl mx-auto w-full">
-      <div className="relative">
-        <AnimeCarousel animes={data?.spotlightAnimes} />
+    <div className="w-full h-auto max-w-[1220px] my-4 mx-auto xl:px-0 sm:px-4 px-2">
+      <div className="flex gap-x-0 items-end">
+        <img src="/assets/nav.gif" alt="logo" width={80} height={72} />
+        <p className="text-6xl font-logo text-secondary-foreground dark:text-white leading-none">
+          ani
+          <span className="text-[#FF003D]">fire</span>
+        </p>
       </div>
 
-      <div className="w-full px-4 my-10">
-        <h2 className="text-2xl text-primary font-semibold mb-6">Trending</h2>
+      <div className="flex w-full items-center gap-x-6">
+        <div className="w-full">
+          <div className="w-full gap-5 h-auto my-8 flex sm:flex-row flex-col">
+            <Input
+              className="rounded-full h-14 text-lg pl-6"
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="One piece"
+            />
+            <Button
+              asChild
+              className="rounded-full h-14 w-full sm:w-14 shrink-0"
+            >
+              <a href={`/search?keyword=${query}`} className={cn(!query && "pointer-events-none opacity-70")}>
+                <SearchIcon className="h-6 w-6" />
+              </a>
+            </Button>
+          </div>
+          <div className="flex w-full">
+            <p>
+              <span className="font-medium mr-2 dark:text-primary-foreground text-secondary-foreground">Top search:</span>
+                  {TopSearchItems.map((item, index) => (
+                    <Link key={item.name + index} href={item.href} className="hover:text-primary duration-300 text-base">
+                      {item.name}{', '}
+                    </Link>
+                  ))}
+            </p>
+          </div>
 
-        <div className="relative lg:pr-16">
-          <ReactSimplyCarousel
-            activeSlideIndex={activeSlideIndex}
-            onRequestChange={setActiveSlideIndex}
-            itemsToShow={6}
-            itemsToScroll={1}
-            swipeTreshold={100}
-            forwardBtnProps={{
-              //here you can also pass className, or any other button element attributes
-              className:
-                "hidden lg:block z-10 absolute right-0 bottom-0 h-[49%]",
-              children: (
-                <span
-                  className={cn(
-                    buttonVariants({
-                      variant: "outline",
-                      className: "h-full w-full",
-                    })
-                  )}
-                >
-                  <GrFormNext className="h-5 w-5" />
-                </span>
-              ),
-            }}
-            backwardBtnProps={{
-              //here you can also pass className, or any other button element attributes
-              className: "hidden lg:block z-10 absolute right-0 top-0 h-[49%]",
-              children: (
-                <span
-                  className={cn(
-                    buttonVariants({
-                      variant: "outline",
-                      className: "h-full w-full",
-                    })
-                  )}
-                >
-                  <GrFormPrevious className="h-5 w-5" />
-                </span>
-              ),
-            }}
-            responsiveProps={[
-              {
-                itemsToShow: 6,
-                itemsToScroll: 3,
-                minWidth: 768,
-                maxWidth: 1536,
-              },
-              {
-                itemsToShow: 1,
-                itemsToScroll: 1,
-                minWidth: 130,
-                maxWidth: 320,
-              },
-              {
-                itemsToShow: 3,
-                itemsToScroll: 1,
-                minWidth: 320,
-                maxWidth: 512,
-              },
-              {
-                itemsToShow: 4,
-                itemsToScroll: 2,
-                minWidth: 512,
-                maxWidth: 768,
-              },
-            ]}
-            speed={400}
-            infinite={false}
-            easing="ease-in-out"
-          >
-            {data?.trendingAnimes.map((anime, index) => {
-              const animeRank = anime.rank ? anime.rank : "00";
-              return (
-                <div
-                  title={anime.name}
-                  className={cn(
-                    "flex items-end w-auto relative pl-3",
-                    index === 0 && "pl-0"
-                  )}
-                  key={anime.id}
-                >
-                  <p
-                    className={cn(
-                      "absolute top-0 z-10 bg-primary text-white grid place-items-center w-8 h-8 md:hidden",
-                      index === 0 ? "left-0" : "left-3"
-                    )}
-                  >
-                    {animeRank}
-                  </p>
-                  <div className="select-none hidden md:block">
-                    <p
-                      className={cn(
-                        "veritcal-text dark:text-white text-secondary-foreground truncate h-52 text-sm font-medium pr-2 pl-2",
-                        index === 0 && "pr-0"
-                      )}
-                    >
-                      {anime.name}
-                    </p>
-                    <p className="text-end pr-3 text-xl dark:text-white text-secondary-foreground mt-4 leading-none font-semibold">
-                      {Number(animeRank) < 10 ? "0" + animeRank : animeRank}
-                    </p>
-                  </div>
+      <Button asChild className="w-full rounded-none mt-3">
+        <Link href="/home">
+          Visit full site
+        </Link>
+      </Button>
+        </div>
 
-                  <a
-                    href={`/${anime.id}`}
-                    className="w-48 user-drag select-none flex-shrink-0 h-64 relative"
-                  >
-                    <img
-                      src={anime.poster}
-                      draggable="false"
-                      alt="anime-name"
-                      className="object-cover absolute pointer-events-none select-none h-full w-full"
-                    />
-                  </a>
-                </div>
-              );
-            })}
-          </ReactSimplyCarousel>
+        <div className="relative hidden lg:block h-[30vw] max-h-72 max-w-[34rem] min-w-[26rem] w-[60vw]">
+          <img
+            src="/assets/nav.gif"
+            alt="hinata shoyo"
+            className="h-full w-full object-contain"
+          />
         </div>
       </div>
 
-      {/* Recently watched! */}
-      <RecentlyWatched />
-
-      <div className="w-full grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 h-auto">
-        <AiringAnime anime={data!} />
-      </div>
-
-      <div className="mt-6 w-full px-4">
-        <div className="flex justify-between items-center">
-          <h6 className="text-2xl text-primary font-semibold">
-            Latest Episode
-          </h6>
-          <p className="flex items-center text-xs gap-x-4">
-            View more <MdArrowForwardIos className="h-3 w-3" />
-          </p>
-        </div>
-
-        <LatestAnimes id={id} latestEpisodes={data?.latestEpisodeAnimes!} />
-      </div>
-
-      {/* Schedule anime here! */}
-      <div className="px-4">
-        <ScheduleAnime />
+      <div className="my-8">
+        <h2 className="text-2xl text-pretty text-secondary-foreground dark:text-primary-foreground font-semibold">AniFire: A website designed exclusively for educational purposes, with no intention of generating revenue or any other commercial pursuits.</h2>
+        <p className="text-muted-foreground text-sm">
+          This website is ad-free and created by an independent developer. Its intended audience is solely for my developer portfolio. If you accessed this site through word-of-mouth, I kindly request you to exit. Thank you for your understanding.
+        </p>
       </div>
     </div>
   );
 };
-export default HomePage;
+export default Home;
